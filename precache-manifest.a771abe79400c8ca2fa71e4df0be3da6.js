f5fcdc3d7dfa731679489012b68fa7bbdef691b1
ertovkax <s280906k@gmail.com>
2021-09-14 17:04:07 +0300

    Updates

---
+++ precache-manifest.a771abe79400c8ca2fa71e4df0be3da6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c318cfb77fed2e815c97f6c79e2f63cc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b2447bdbd06854934a11",
+    "url": "./static/js/2.27babbfc.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.27babbfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5cbbe1d281c7f8a94a7",
+    "url": "./static/js/main.73a35c2d.chunk.js"
+  },
+  {
+    "revision": "cf45432d129a816c05f6",
+    "url": "./static/js/runtime-main.dd0e4089.js"
+  }
+]);
